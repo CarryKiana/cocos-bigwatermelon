a07e29d0833931751d9327a3ab7e8c552362dd78
yukairui <825294867@qq.com>
2022-01-07 18:07:57 +0800

    碰撞逻辑处理没想明白

--- assets/scripts/GameManager.ts
+++ assets/scripts/GameManager.ts
@@ -36,6 +36,8 @@ export class GameManager extends Component {
     private _randomLength = 6
     private _size = 20
     private _FruitStatus = FruitStatus.INIT
+    private _index = null
+    private _timer = null
 
     @property({ type: Node })
     public playPanel = null
@@ -69,66 +71,103 @@ export class GameManager extends Component {
         LabelComp.string = 'X ' + this._ComboValue
         this.ComboLabel.active = false
 
-        this.geneFruit(true)
-        systemEvent.on(SystemEvent.EventType.TOUCH_START, this.moveFruit, this)
+        this._FruitStatus = FruitStatus.INIT
+        this.geneFruit(true, null, null)
+
+        systemEvent.on(SystemEvent.EventType.TOUCH_START, this.startMoveFruit, this)
+        systemEvent.on(SystemEvent.EventType.TOUCH_MOVE, this.keepMoveFruit, this)
+        systemEvent.on(SystemEvent.EventType.TOUCH_END, this.drop, this)
     }
     // 生成水果
-    geneFruit (first: boolean) {
-        this._FruitStatus = FruitStatus.INIT
+    geneFruit (first: boolean, location: Vec2 | null, target: number | null) {
         this._currentFruit = instantiate(this.fruitPrefab)
-        const index = first ? 0 : (Math.random() * this._randomLength | 0)
+        this._currentFruit.parent = this.playPanel
+
+        if (location) {
+            this._currentFruit.setWorldPosition(new Vec3(location.x, location.y, 0))
+            this._currentFruit.getChildByName('img').position = new Vec3(0,0,0)
+        }
+        const index = target || (first ? 0 : (Math.random() * this._randomLength | 0))
+        this._currentFruit.getComponent('Fruit').index = index
+        console.log('生成的', index)
         this._currentFruit.getChildByName('img').getComponent(Sprite).spriteFrame = this.fruits[index].iconSF
         const uiTransform = this._currentFruit.getChildByName('img').getComponent(UITransform)
-        console.log(uiTransform)
         uiTransform.width = uiTransform.height = this._size * (1 + index / 5)
         const circleCollider2D = this._currentFruit.getChildByName('img').getComponent(CircleCollider2D)
-        
         circleCollider2D.radius =  this._size * (1 + index / 5)/2
         circleCollider2D.on(Contact2DType.BEGIN_CONTACT, this.onTriggerEnter, this)
-        this._currentFruit.parent = this.playPanel
+        circleCollider2D.apply()
     }
     // 鼠标按下--移动水果事件处理
-    moveFruit (e) {
-        console.log(e)
-        console.log(e.getLocationX())
+    startMoveFruit (e) {
         if (this._currentFruit && this._FruitStatus === FruitStatus.INIT ) {
-            // this._currentFruit.setWorldPosition(new Vec3(0, this._currentFruit.position.y, 0))
-            tween(this._currentFruit.getChildByName('img'))
-                .to(3, { position: new Vec3(100 ,0, 0) }, { onComplete: () => {
+            tween(this._currentFruit)
+                .to(0.1, { worldPosition: new Vec3(e.getLocationX()/2, this._currentFruit.getWorldPosition().y, 0) }, { onComplete: () => {
                     console.log(this._currentFruit.position)
                     console.log(this._currentFruit)
+                }, onUpdate: () => {
+                    this._currentFruit.getChildByName('img').position = new Vec3(0,0,0)
                 } })
                 .start()
+        }
+    }
+    // 鼠标拖动 -- 变更水果位置
+    keepMoveFruit (e) {
+        if (this._currentFruit && this._FruitStatus === FruitStatus.INIT ) {
+            this._currentFruit.setWorldPosition(new Vec3(e.getLocationX()/2, this._currentFruit.getWorldPosition().y, 0))
+            this._currentFruit.getChildByName('img').position = new Vec3(0,0,0)
         }
     }
     // 鼠标放开--开始自由落体
     drop () {
         if (this._currentFruit && this._FruitStatus === FruitStatus.INIT ) {
             this._FruitStatus = FruitStatus.DROPPING
-            console.log(this._currentFruit)
             const rigidBody2D = this._currentFruit.getChildByName('img').getComponent(RigidBody2D)
-            rigidBody2D.gravityScale = 2
-            console.log(rigidBody2D)
+            rigidBody2D.gravityScale = 3
             rigidBody2D.linearVelocity = new Vec2( Math.random() + -0.5 , 1)
-            
         }
     }
-    // 水果碰检测
+    // 水果碰撞检测
     onTriggerEnter (selfCollider:Collider2D, otherCollider:Collider2D, contact: IPhysics2DContact | null) {
         console.log(selfCollider)
         console.log(otherCollider)
-        if (otherCollider instanceof BoxCollider2D) {
-            // 撞墙不做处理
+        console.log(selfCollider.node.parent.getComponent('Fruit')['index'])
+        if (otherCollider instanceof BoxCollider2D  && otherCollider.node.name !== 'background-border') {
+            // 撞两边墙不做处理
         } else {
             console.log('检查是否是相同水果,合并相同水果')
+            // 相同水果的情况下，消除相同水果，在碰撞点生成一个上级水果自由下落
+            // otherCollider.node.parent.destroy()
+            // 非相同水果不做处理
+            // 碰撞完成判断游戏是否结束，结束显示弹窗
+            // 未结束重新生成水果
+            this._currentFruit.getChildByName('img').getComponent(CircleCollider2D).off(Contact2DType.BEGIN_CONTACT, this.onTriggerEnter, this)
+            if (otherCollider.node.name === 'background-border' || selfCollider.node.parent.getComponent('Fruit')['index'] !== otherCollider.node.parent.getComponent('Fruit')['index'] ) {
+                this._currentFruit = null
+                this._FruitStatus = FruitStatus.DEAD
+                clearTimeout(this._timer)
+                 this._timer = setTimeout(_ => {
+                    this._FruitStatus = FruitStatus.INIT
+                    this.geneFruit(false, null, null)
+                })
+            } else {
+                const worldManifold =  contact.getWorldManifold()
+                const points = worldManifold.points
+                const normal = worldManifold.normal
+                setTimeout(_ => {
+                    const next = selfCollider.node.parent.getComponent('Fruit')['index'] + 1
+                    selfCollider.node.parent.removeFromParent()
+                    otherCollider.node.parent.removeFromParent()
+                    clearTimeout(this._timer)
+                    this.geneFruit(false, points[0], next)
+                    const rigidBody2D = this._currentFruit.getChildByName('img').getComponent(RigidBody2D)
+                    rigidBody2D.gravityScale = 3
+                    rigidBody2D.linearVelocity = new Vec2( Math.random() + -0.5 , 1)
+                })
+            }
+            // selfCollider.node.parent.destroy()
+
         }
-        // 碰撞完成重新生成水果(游戏未结束情况下)
-        this._currentFruit.getChildByName('img').getComponent(CircleCollider2D).off(Contact2DType.BEGIN_CONTACT, this.onTriggerEnter, this)
-        this._currentFruit = null
-        this._FruitStatus = FruitStatus.DEAD
-        setTimeout(_ => {
-            this.geneFruit(false)
-        })
     }
     // update (deltaTime: number) {
     //     // [4]
